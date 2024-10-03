28b00a9c672e03103da7a85d08975f01811fd83e
ttttux <bi_tux1510@protonmail.com>
2024-10-03 15:14:12 +0200

    added game over screen

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,9 @@
     extern crate core;
 
     use std::random::random;
+    use std::thread;
     use std::time::{Duration, SystemTime};
+    use bevy::color::palettes::css::{CRIMSON, WHITE};
     use bevy::ecs::system::Adapt;
     use bevy::math::NormedVectorSpace;
     use bevy::prelude::*;
@@ -17,9 +19,13 @@
         App::new()
             .add_plugins(DefaultPlugins.set(ImagePlugin::default_nearest())) // prevents blurry sprites
             .add_plugins(bevy_framepace::FramepacePlugin)
+            .init_state::<GameState>()
+            .init_state::<MenuState>()
             .add_systems(Startup, setup)
             .add_systems(Startup, splash_setup)
-            .add_systems(Startup, countdown)
+            .add_systems(Startup, menu_setup)
+            .add_systems(OnEnter(GameState::Over), main_menu_setup)
+            .add_systems(Update, countdown.after(splash_setup))
             .add_systems(Update, execute_animations)
             .add_systems(Update, player_movement_system)
             .add_systems(Update, player_weapons_system)
@@ -30,7 +36,8 @@
             .add_systems(Update, player_kill_system)
             .add_systems(Update, enemy_spawn_system)
             .add_systems(Update, spawn_timer_system)
-            .run();
+            .add_systems(Update, (menu_action, button_system).run_if(in_state(GameState::Over)))
+        .run();
     }
 
     fn trigger_animation<S: Component>(mut query: Query<&mut AnimationConfig, With<S>>) {
@@ -80,14 +87,63 @@
         }
     }
 
+    #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+    enum MenuState {
+        Main,
+        Settings,
+        SettingsDisplay,
+        SettingsSound,
+        #[default]
+        Disabled,
+    }
+
+    // Tag component used to tag entities added on the main menu screen
     #[derive(Component)]
-    #[derive(Default)]
-    enum GameStateEnum {
+    struct OnMainMenuScreen;
+
+    // Tag component used to tag entities added on the settings menu screen
+    #[derive(Component)]
+    struct OnSettingsMenuScreen;
+
+    // Tag component used to tag entities added on the display settings menu screen
+    #[derive(Component)]
+    struct OnDisplaySettingsMenuScreen;
+
+    // Tag component used to tag entities added on the sound settings menu screen
+    #[derive(Component)]
+    struct OnSoundSettingsMenuScreen;
+
+    const NORMAL_BUTTON: Color = Color::srgb(0.15, 0.15, 0.15);
+    const HOVERED_BUTTON: Color = Color::srgb(0.25, 0.25, 0.25);
+    const HOVERED_PRESSED_BUTTON: Color = Color::srgb(0.25, 0.65, 0.25);
+    const PRESSED_BUTTON: Color = Color::srgb(0.35, 0.75, 0.35);
+
+    // Tag component used to mark which setting is currently selected
+    #[derive(Component)]
+    struct SelectedOption;
+
+    // All actions that can be triggered from a button click
+    #[derive(Component)]
+    enum MenuButtonAction {
+        Play,
+        Settings,
+        SettingsDisplay,
+        SettingsSound,
+        BackToMainMenu,
+        BackToSettings,
+        Quit,
+    }
+
+
+    #[derive(Clone, Copy, Default, Eq, PartialEq, Debug, Hash, States)]
+    enum GameState {
         #[default]
         Splash,
         Menu,
         Game,
+        Over
     }
+
 
 
     #[derive(Component)]
@@ -442,6 +498,7 @@
 
 
     fn player_kill_system(
+        mut game_state: ResMut<NextState<GameState>>,
         mut commands: Commands,
         mut query: Query<(Entity, &Transform), With<Player>>,
         enemy_query: Query<&Transform, With<Enemy>>,
@@ -477,6 +534,8 @@
                             frame_timer: 0.0
                         }
                     ));
+
+                    game_state.set(GameState::Over);
                 }
             }
         }
@@ -529,22 +588,16 @@
         }
     }
 
-    // Splash screen logic
     #[derive(Component)]
     struct OnSplashScreen;
 
+    // Newtype to use a `Timer` for this screen as a resource
     #[derive(Resource, Deref, DerefMut)]
     struct SplashTimer(Timer);
 
-    fn splash_setup(
-        mut commands: Commands,
-        asset_server: Res<AssetServer>,
-    ) {
-        commands.spawn(
-            GameStateEnum::Splash
-        );
-
+    fn splash_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
         let icon = asset_server.load("branding/icon.png");
+        // Display the logo
         commands
             .spawn((
                 NodeBundle {
@@ -562,6 +615,7 @@
             .with_children(|parent| {
                 parent.spawn(ImageBundle {
                     style: Style {
+                        // This will set the logo to be 200px wide, and auto adjust its height
                         width: Val::Px(200.0),
                         ..default()
                     },
@@ -572,20 +626,179 @@
         commands.insert_resource(SplashTimer(Timer::from_seconds(1.0, TimerMode::Once)));
     }
 
+
     fn countdown(
-        mut game_state_query: Query<&mut GameStateEnum>,
+        mut commands: Commands,
+        mut game_state: ResMut<NextState<GameState>>,
         time: Res<Time>,
+        mut timer: Option<ResMut<SplashTimer>>,
+        mut query: Query<Entity, With<UiImage>>
     ) {
-        let mut game_state = game_state_query.single_mut();
-        if SplashTimer.tick(time.delta()).finished() {
-            let mut pinned = std::pin::pin!(game_state.as_mut());
-            pinned.as_mut().set(&mut GameStateEnum::Menu);
-        }
+            if let Some(mut timer) = timer {
+                if timer.tick(time.delta()).finished() {
+                    game_state.set(GameState::Game);
+                    for image in &mut query {
+                        commands.entity(image).despawn();
+                    }
+                }
+            } else {
+                eprintln!("SplashTimer resource not found!");
+            }
     }
 
-    // Game screen logic
     #[derive(Component)]
     struct OnGameScreen;
 
     #[derive(Resource, Deref, DerefMut)]
     struct GameTimer(Timer);
+
+    fn button_system(
+        mut interaction_query: Query<
+            (&Interaction, &mut UiImage, Option<&SelectedOption>),
+            (Changed<Interaction>, With<Button>),
+        >,
+    ) {
+        for (interaction, mut image, selected) in &mut interaction_query {
+            image.color = match (*interaction, selected) {
+                (Interaction::Pressed, _) | (Interaction::None, Some(_)) => PRESSED_BUTTON,
+                (Interaction::Hovered, Some(_)) => HOVERED_PRESSED_BUTTON,
+                (Interaction::Hovered, None) => HOVERED_BUTTON,
+                (Interaction::None, None) => NORMAL_BUTTON,
+            }
+        }
+    }
+
+    fn menu_action(
+        interaction_query: Query<
+            (&Interaction, &MenuButtonAction),
+            (Changed<Interaction>, With<Button>),
+        >,
+        mut app_exit_events: EventWriter<AppExit>,
+        mut menu_state: ResMut<NextState<MenuState>>,
+        mut game_state: ResMut<NextState<GameState>>,
+    ) {
+        for (interaction, menu_button_action) in &interaction_query {
+            if *interaction == Interaction::Pressed {
+                match menu_button_action {
+                    MenuButtonAction::Quit => {
+                        app_exit_events.send(AppExit::Success);
+                    }
+                    MenuButtonAction::Play => {
+                        game_state.set(GameState::Game);
+                        menu_state.set(MenuState::Disabled);
+
+                    }
+                    MenuButtonAction::Settings => menu_state.set(MenuState::Settings),
+                    MenuButtonAction::SettingsDisplay => {
+                        menu_state.set(MenuState::SettingsDisplay);
+                    }
+                    MenuButtonAction::SettingsSound => {
+                        menu_state.set(MenuState::SettingsSound);
+                    }
+                    MenuButtonAction::BackToMainMenu => menu_state.set(MenuState::Main),
+                    MenuButtonAction::BackToSettings => {
+                        menu_state.set(MenuState::Settings);
+                    }
+                }
+            }
+        }
+    }
+
+    fn menu_setup(mut menu_state: ResMut<NextState<MenuState>>) {
+        menu_state.set(MenuState::Main);
+    }
+
+    fn main_menu_setup(mut commands: Commands, asset_server: Res<AssetServer>) {
+        // Common style for all buttons on the screen
+        let button_style = Style {
+            width: Val::Px(250.0),
+            height: Val::Px(65.0),
+            margin: UiRect::all(Val::Px(20.0)),
+            justify_content: JustifyContent::Center,
+            align_items: AlignItems::Center,
+            ..default()
+        };
+        let button_icon_style = Style {
+            width: Val::Px(30.0),
+            // This takes the icons out of the flexbox flow, to be positioned exactly
+            position_type: PositionType::Absolute,
+            // The icon will be close to the left border of the button
+            left: Val::Px(10.0),
+            ..default()
+        };
+        let button_text_style = TextStyle {
+            font_size: 40.0,
+            color: WHITE.into(),
+            ..default()
+        };
+
+        commands
+            .spawn((
+                NodeBundle {
+                    style: Style {
+                        width: Val::Percent(100.0),
+                        height: Val::Percent(100.0),
+                        align_items: AlignItems::Center,
+                        justify_content: JustifyContent::Center,
+                        ..default()
+                    },
+                    ..default()
+                },
+                OnMainMenuScreen,
+            ))
+            .with_children(|parent| {
+                parent
+                    .spawn(NodeBundle {
+                        style: Style {
+                            flex_direction: FlexDirection::Column,
+                            align_items: AlignItems::Center,
+                            ..default()
+                        },
+                        background_color: CRIMSON.into(),
+                        ..default()
+                    })
+                    .with_children(|parent| {
+                        // Display the game name
+                        parent.spawn(
+                            TextBundle::from_section(
+                                "Game Over",
+                                TextStyle {
+                                    font_size: 80.0,
+                                    color: WHITE.into(),
+                                    ..default()
+                                },
+                            )
+                                .with_style(Style {
+                                    margin: UiRect::all(Val::Px(50.0)),
+                                    ..default()
+                                }),
+                        );
+
+                        // Display three buttons for each action available from the main menu:
+                        // - new game
+                        // - settings
+                        // - quit
+                        parent
+                            .spawn((
+                                ButtonBundle {
+                                    style: button_style.clone(),
+                                    background_color: NORMAL_BUTTON.into(),
+                                    ..default()
+                                },
+                                MenuButtonAction::Play,
+                            ))
+                            .with_children(|parent| {
+                                let icon = asset_server.load("textures/Game Icons/          Arch-linux-logo.png");
+                                parent.spawn(ImageBundle {
+                                    style: button_icon_style.clone(),
+                                    image: UiImage::new(icon),
+                                    ..default()
+                                });
+                                parent.spawn(TextBundle::from_section(
+                                    "New Game",
+                                    button_text_style.clone(),
+                                ));
+                            });
+                    });
+            });
+    }
